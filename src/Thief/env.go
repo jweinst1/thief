5f94d5c4748912c7ee87f3fe9da91c89afcefbdf
JoshuaWeinstein <jweinst1@berkeley.edu>
2016-12-25 15:07:13 -0800

    glee set

--- src/Thief/env.go
+++ src/Thief/env.go
@@ -1,7 +1,36 @@
 package main
 
+import "fmt"
+
 //file for the env struct
 
 type Env struct {
-	
+	items map[string]interface{}
+}
+
+func NewEnv() Env {
+	return Env{make(map[string]interface{})}
+}
+
+//general getter
+func (self *Env) get(key string) interface{} {
+	val, has := self.items[key]
+	if has {
+		return val
+	} else {
+		return nil
+	}
+}
+
+//general setter
+func (self *Env) set(key string, value interface{}) {
+	self.items[key] = value
+}
+
+func main() {
+	g := NewEnv()
+	g.set("foo", 4)
+	g.set("hello", false)
+	fmt.Println(g)
+
 }
