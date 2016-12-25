763eeef9e79def93d36661c63addb8ba8bc5a8fe
JoshuaWeinstein <jweinst1@berkeley.edu>
2016-12-25 13:39:34 -0800

    new byte to s

--- src/Thief/butil.go
+++ src/Thief/butil.go
@@ -9,3 +9,7 @@ func Byte_to_i(in []byte) int {
     //must be length 4
     return  int((int32(in[0]) << 24 | int32(in[1]) << 16 | int32(in[2]) << 8 | int32(in[3])))
 }
+
+func Byte_to_s(in []byte) string {
+	return string(in)
+}
