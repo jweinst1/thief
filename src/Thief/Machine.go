491dca8e0255767942d10e1a9bd8ac3de49496d0
JoshuaWeinstein <jweinst1@berkeley.edu>
2016-12-25 22:48:52 -0800

    machine file

---
+++ src/Thief/Machine.go
@@ -0,0 +1,13 @@
+package main
+
+//file for Machine struct and methods
+
+type Machine struct {
+	env Env
+	//print register
+}
+
+//main method for reading/running bytes
+func (self *Machine) run(arg byte){
+
+}
