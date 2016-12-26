0f2b542dddd6663b3ef9cfbfe6e32f07866531f6
JoshuaWeinstein <jweinst1@berkeley.edu>
2016-12-26 10:28:10 -0800

    print arm

--- src/Thief/arms.go
+++ src/Thief/arms.go
@@ -1,8 +1,30 @@
 package main
 
+import "fmt"
+
 //subregisters with specialized behavior
 //these are called arms
 
-type Arm interface {
+
+
+type Printarm struct {
+	val interface{}
+}
+
+func NewPrintArm(arg interface{}) Printarm {
+	return Printarm{arg}
+}
+
+func (self *Printarm) put(value interface{}){
+	self.val = value
+}
+
+func (self *Printarm) print(){
+	fmt.Println(self.val)
+}
+
+
+
+func main(){
 	
 }
